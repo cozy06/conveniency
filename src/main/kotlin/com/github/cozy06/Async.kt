e74905269e28b6eb8fc3698495ae7f571696ee75
cozy06 <jamesjin06@gmail.com>
2023-03-27 01:56:22 +0900

    delay function

---
+++ src/main/kotlin/com/github/cozy06/Async.kt
@@ -0,0 +1,14 @@
+package com.github.cozy06
+
+import kotlin.concurrent.thread
+
+class Async {
+    fun delay(millis: Long, function: () -> Unit, repeat: Int = 1) {
+        thread(start = true) {
+            for(i: Int in 1..repeat) {
+                Thread.sleep(millis)
+                function()
+            }
+        }
+    }
+}
--- src/main/kotlin/com/github/cozy06/Delay.kt
+++
@@ -1,4 +0,0 @@
-package com.github.cozy06
-
-class Delay {
-}
