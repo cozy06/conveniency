1594953f16952cf21b20b84a87e885adcbfe1ad3
cozy06 <jamesjin06@gmail.com>
2023-03-28 17:51:04 +0900

    repeat function

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,10 +2,11 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.8.10"
+    `maven-publish`
 }
 
-group = "com.github.cozy06"
-version = "1.0-SNAPSHOT"
+group = "io.github.cozy06"
+version = "1.0.0"
 
 repositories {
     mavenCentral()
@@ -13,6 +14,21 @@ repositories {
 
 dependencies {
     testImplementation(kotlin("test"))
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            from(components["java"])
+        }
+    }
+
+    repositories {
+        maven {
+            name = "jitpack"
+            url = uri("https://jitpack.io")
+        }
+    }
 }
 
 tasks.test {
---
+++ src/main/kotlin/com/github/cozy06/Repeat.kt
@@ -0,0 +1,20 @@
+package com.github.cozy06
+
+class Repeat {
+    fun repeat(action: (Int) -> Unit, times: Int, startValue: Int = 0, step: Int = 1) {
+        if(step == 0) throw IllegalArgumentException("Step Can't Be ZERO")
+        var index = startValue
+        if(step < 0) {
+            while(times * step + startValue < index) {
+                action(index)
+                index += step
+            }
+        }
+        else {
+            while(times * step + startValue > index) {
+                action(index)
+                index += step
+            }
+        }
+    }
+}
