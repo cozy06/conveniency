60e19a653b7b9c9a9cfa8de219842b450dcb9953
cozy06 <jamesjin06@gmail.com>
2023-03-30 10:12:15 +0900

    update class structure

--- src/main/kotlin/com/github/cozy06/Repeat.kt
+++ src/main/kotlin/com/github/cozy06/Repeat.kt
@@ -1,19 +1,20 @@
 package com.github.cozy06
 
 class Repeat {
-    fun loop(action: (Int) -> Unit, times: Int, startValue: Int = 0, step: Int = 1) {
-        if(step == 0) throw IllegalArgumentException("Step Can't Be ZERO")
-        var index = startValue
-        if(step < 0) {
-            while(times * step + startValue < index) {
-                action(index)
-                index += step
-            }
-        }
-        else {
-            while(times * step + startValue > index) {
-                action(index)
-                index += step
+    companion object {
+        fun loop(action: (Int) -> Unit, times: Int, startValue: Int = 0, step: Int = 1) {
+            if (step == 0) throw IllegalArgumentException("Step Can't Be ZERO")
+            var index = startValue
+            if (step < 0) {
+                while (times * step + startValue < index) {
+                    action(index)
+                    index += step
+                }
+            } else {
+                while (times * step + startValue > index) {
+                    action(index)
+                    index += step
+                }
             }
         }
     }
