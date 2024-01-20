4304a4580223b831c3a01253d5862a1c0e4228b1
cozy06 <jamesjin06@gmail.com>
2024-01-20 21:07:50 +0900

    loop 함수 변경

--- src/main/kotlin/com/github/cozy06/Logic.kt
+++ src/main/kotlin/com/github/cozy06/Logic.kt
@@ -2,19 +2,10 @@ package com.github.cozy06
 
 class Logic {
     companion object {
-        fun loop(action: (Int) -> Unit, times: Int, startValue: Int = 0, step: Int = 1) {
-            if (step == 0) throw IllegalArgumentException("Step Can't Be ZERO")
-            var index = startValue
-            if (step < 0) {
-                while (times * step + startValue < index) {
-                    action(index)
-                    index += step
-                }
-            } else {
-                while (times * step + startValue > index) {
-                    action(index)
-                    index += step
-                }
+        fun loop(list: MutableList<Any>, action: (Any) -> Unit) {
+            repeat(list.size) {
+                val index = list[it]
+                action(index)
             }
         }
 
